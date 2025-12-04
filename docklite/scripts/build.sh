bb5fbbda8e32078c69fd0d48ff2f93d6604e83d6
Juicywoowowow <Brothersheyguys@gmail.com>
2025-12-04 21:47:45 +0800

    clean up

--- docklite/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Building docklite..."
-make clean
-make all
-
-echo "Build complete!"
--- docklite/src/core/config_parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_PARSER_H
-#define CONFIG_PARSER_H
-
-#include "utils/common.h"
-
-int parse_docklite_config(const char *filepath, ContainerConfig *config);
-void free_container_config(ContainerConfig *config);
-
-#endif
